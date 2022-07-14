5e36c78c68ca7ddf59ba337dab281d2084899eeb
ItsGoldeneyes <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-07-14 00:45:20 -0400

    h

--- src/logic.py
+++ src/logic.py
@@ -68,7 +68,12 @@ def choose_move(data: dict) -> str:
     #         move = random.choice(possible_moves)
     # else:
     #     move = random.choice(possible_moves)
-    move = random.choice(possible_moves)
+    if len(possible_moves) == 0:
+        move = "up"
+    elif len(possible_moves) == 1:
+        move = possible_moves[0]
+    else:
+        move = random.choice(possible_moves)
 
     print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
 
@@ -77,7 +82,7 @@ def choose_move(data: dict) -> str:
 
 def _filter_wall_moves(my_body: dict, possible_moves: List[str], board: dict) -> List[str]:
     my_head = my_body[0]
-
+    print("\n")
     board_height = board['height']
     board_width = board['width']
 
@@ -87,17 +92,26 @@ def _filter_wall_moves(my_body: dict, possible_moves: List[str], board: dict) ->
         "left": {"x": my_head['x']-1, "y": my_head['y']},
         "right": {"x": my_head['x']+1, "y": my_head['y']}
     }
+    print(possible_moves)
     print(moves)
+    
+    to_remove = []
+    
     for direction in possible_moves:
-
+        print(direction)
         # Don't hit walls
-            if 0 == moves[direction]["x"] or moves[direction]["x"] == board_width:
-                possible_moves.remove(direction)
-            if 0 == moves[direction]["y"] or moves[direction]["y"] == board_height:
-                possible_moves.remove(direction)
+        if -1 == moves[direction]["x"] or moves[direction]["x"] == board_width:
+            to_remove.append(direction)
+        elif -1 == moves[direction]["y"] or moves[direction]["y"] == board_height:
+            to_remove.append(direction)
+    
+    for i in to_remove:
+        if i in possible_moves:
+            possible_moves.remove(i)
+    
 
     print(possible_moves)
-    print("\n")
+    
     return possible_moves
 
 
@@ -110,11 +124,15 @@ def _filter_self_moves(my_body: dict, possible_moves: List[str]) -> List[str]:
         "left": {"x": my_head['x']-1, "y": my_head['y']},
         "right": {"x": my_head['x']+1, "y": my_head['y']}
     }
-
+    to_remove = []
     for direction in possible_moves:
         for segment in my_body[1:]:
             if segment == moves[direction]:
-                possible_moves.remove(direction)
+                to_remove.append(direction)
+                
+    for i in to_remove:
+        if i in possible_moves:
+            possible_moves.remove(i)
 
     return possible_moves
 
