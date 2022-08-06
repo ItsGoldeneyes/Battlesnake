450a92b2df265dfbdb8573d879758774fea30293
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-08-05 23:32:04 -0400

    customization

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def handle_info():
             "apiversion": "1",
             "author": "Goldeneyes",
             "color": "#00FFFF",  # "color": "#EB6443",
-            "self.head": "bender",
+            "self.head": "bendr",
             "tail": "cosmic-horror",
         }
 
