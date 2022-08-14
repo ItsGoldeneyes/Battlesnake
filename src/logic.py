845d574a6257b8ada4bdd8ecda4111c5dd2682eb
ItsGoldeneyes <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-08-13 21:09:49 -0400

    implement  floodfill

--- src/logic.py
+++ src/logic.py
@@ -22,14 +22,17 @@ class LogicSnake(BattleSnake):
     def choose_move(self):
         potential_moves = self.find_moves(self.get_head())
         #Filter collision moves
+        
         alive_moves = {move : potential_moves[move] for move in potential_moves 
                        if self.board.collision_check(potential_moves[move])==False}
+        
         if alive_moves == {}:
             return "up"
         
         move_choice = {}
         flood_scores = {move:self.flood_fill(self.board, alive_moves[move], []) for move in alive_moves}
         
+        # If coming up to intersection
         if len(set(flood_scores.values())) != 1:
             print("flood:", flood_scores)
             move_choice = max(flood_scores, key=flood_scores.get)
@@ -37,14 +40,16 @@ class LogicSnake(BattleSnake):
         # Avoid food if can
         elif self.board.get_health(self.id) > 50:
             no_food_moves = {move : potential_moves[move] for move in potential_moves 
-                if self.board.avoid_food(potential_moves[move])==False}
+                if self.board.avoid_food(potential_moves[move])==False and self.board.collision_check(potential_moves[move])==False}
             if no_food_moves != {}:
                 alive_moves = no_food_moves
+            move_choice = random.choice(list(alive_moves.keys()))  
                 
         else:
             move_choice = random.choice(list(alive_moves.keys()))
-            
-        if self.board.get_head(self.id) < 10:
+        
+        # If hungry    
+        if self.board.get_health(self.id) < 10:
             food_dists = self.board.food_dist(self.get_head(), alive_moves)
             if len(set(food_dists.values())) != 1:
                 move_choice = min(food_dists, key=food_dists.get)
