52ee91cf6b69de77033eedfad52987e232e97bf9
ItsGoldeneyes <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-08-13 20:56:18 -0400

    return

--- src/logic.py
+++ src/logic.py
@@ -27,23 +27,28 @@ class LogicSnake(BattleSnake):
         if alive_moves == {}:
             return "up"
         
-        if self.board.get_health(self.id) > 50:
-                alive_moves = {move : potential_moves[move] for move in potential_moves 
-                        if self.board.avoid_food(potential_moves[move])==False}
-                if alive_moves == {}:
-                    alive_moves = {move : potential_moves[move] for move in potential_moves 
-                                if self.board.collision_check(potential_moves[move])==False}
-        #Get Floodfill scores
+        move_choice = {}
         flood_scores = {move:self.flood_fill(self.board, alive_moves[move], []) for move in alive_moves}
-        move_choice = random.choice(list(alive_moves.keys()))
+        
         if len(set(flood_scores.values())) != 1:
+            print("flood:", flood_scores)
             move_choice = max(flood_scores, key=flood_scores.get)
+        
+        # Avoid food if can
+        elif self.board.get_health(self.id) > 50:
+            no_food_moves = {move : potential_moves[move] for move in potential_moves 
+                if self.board.avoid_food(potential_moves[move])==False}
+            if no_food_moves != {}:
+                alive_moves = no_food_moves
+                
+        else:
+            move_choice = random.choice(list(alive_moves.keys()))
             
-        if self.board.get_health(self.id) < 10:
+        if self.board.get_head(self.id) < 10:
             food_dists = self.board.food_dist(self.get_head(), alive_moves)
             if len(set(food_dists.values())) != 1:
                 move_choice = min(food_dists, key=food_dists.get)
-            
         
-            
+        print(move_choice)
+        print("")
         return move_choice
