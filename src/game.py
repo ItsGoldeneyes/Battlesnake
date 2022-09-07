390daef6dc8c40e13cb318ff9593896075510745
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-09-07 15:58:16 -0400

    not random for other modes

--- src/game.py
+++ src/game.py
@@ -39,17 +39,20 @@ class Game:
         elif self.rules == "standard":
             move_type = StandardMove(self.board)
             move = move_type.choose_move(self.board.snakes[self.board.get_self_id()], depth= 3)
+        
         else:
-            potential_moves = self.board.find_moves(self.get_head())
-            alive_moves = {move : potential_moves[move] for move in potential_moves if self.board.collision_check(potential_moves[move])==False}
-            if alive_moves == {}:
-                return "up"
-            if self.board.get_health(self.id) < 30:
-                food_dists = self.board.food_dist(self.get_head(), alive_moves)
-                move_choice = min(food_dists, key=food_dists.get)
-            else:
-                move_choice = random.choice(list(alive_moves.keys()))
-            move = move_choice
+            move_type = StandardMove(self.board)
+            move = move_type.choose_move(self.board.snakes[self.board.get_self_id()], depth= 3)
+            # potential_moves = self.board.find_moves(self.get_head())
+            # alive_moves = {move : potential_moves[move] for move in potential_moves if self.board.collision_check(potential_moves[move])==False}
+            # if alive_moves == {}:
+            #     return "up"
+            # if self.board.get_health(self.id) < 30:
+            #     food_dists = self.board.food_dist(self.get_head(), alive_moves)
+            #     move_choice = min(food_dists, key=food_dists.get)
+            # else:
+            #     move_choice = random.choice(list(alive_moves.keys()))
+            # move = move_choice
         
         if move in self.possible_moves and move != None:
             return move
