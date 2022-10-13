f7c54d4ecf683ea8485e61651f99837d59ebe5eb
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-10-13 16:07:36 -0400

    s

--- src/main.py
+++ src/main.py
@@ -7,13 +7,9 @@ import os
 
 from game import Game
 
-print(os.getenv('DEBUG_MODE', False))
-print(os.getenv('MOVE_MODE', False))
-print(os.getenv('TIMING_MODE', False))
-
-DEBUG_MODE = bool(os.getenv('DEBUG_MODE', False))
-MOVE_MODE = bool(os.getenv('MOVE_MODE', False))
-TIMING_MODE = bool(os.getenv('TIMING_MODE', False))
+DEBUG_MODE = os.getenv('DEBUG_MODE', False)=="True"
+MOVE_MODE = os.getenv('MOVE_MODE', False)=="True"
+TIMING_MODE = os.getenv('TIMING_MODE', False)=="True"
 
 print(DEBUG_MODE)
 print(MOVE_MODE)
